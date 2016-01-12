7d35191964d3ffe04e4de4e03a0a9ef8cf7a6ce1
Do Lee <do.lee.y@gmail.com>
2016-01-12 02:37:33 -0800

    updated up to Q5 in 05a-python.md

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,43 @@
 # Hint:  use Google to find python function
 
 ####a) 
+from datetime import datetime
+
+## date strings
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+
+## convert to dates
+date1 = datetime.strptime(date_start, '%m-%d-%Y').date()
+date2 = datetime.strptime(date_stop, '%m-%d-%Y').date()
+
+## calc diff days
+(date2 - date1).days
+
 
 ####b)  
+from datetime import datetime
+
 date_start = '12312013'  
 date_stop = '05282015'  
 
-####c)  
+## convert to dates
+date1 = datetime.strptime(date_start, '%m%d%Y').date()
+date2 = datetime.strptime(date_stop, '%m%d%Y').date()
+
+## calc diff days
+(date2 - date1).days
+
+
+####c)
+from datetime import datetime
+
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+## convert to dates
+date1 = datetime.strptime(date_start, '%d-%b-%Y').date()
+date2 = datetime.strptime(date_stop, '%d-%b-%Y').date()
+
+## calc diff days
+(date2 - date1).days
